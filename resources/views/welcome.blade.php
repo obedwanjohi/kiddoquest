c12e8f9d00bbbff9f982d2d336a82668b69b2dcf
josewanjo11 <68461423+josewanjo11@users.noreply.github.com>
2026-08-31 11:03:13 +0300

    Clean welcome.blade.php attributes and add view cache clear

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -120,7 +120,7 @@
     </style>
 </head>
 
-<body class="bg-[#FAF5FF] text-slate-900 antialiased overflow-x-hidden" x-data="{ authModal: {{ $errors->any() ? 'true' : 'false' }}, authTab: '{{ $errors->has('name') ? 'register' : 'login' }}', mobileMenu: false }">
+<body class="bg-[#FAF5FF] text-slate-900 antialiased overflow-x-hidden" x-data="{ authModal: false, authTab: 'login', mobileMenu: false }">
 
     {{-- Error Toast Notification --}}
     @if($errors->any())
